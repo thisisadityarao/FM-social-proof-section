1ba1dd74b0e738c9c4ef3f4640e87f865d7bf28e
Aditya Rao <dev.adityarao@gmail.com>
2024-05-29 21:05:28 +0530

    Complete mobile styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,27 @@
 export default {
   content: ["./index.html"],
   theme: {
+    fontFamily: {
+      sans: ["League Spartan", "sans-serif"],
+    },
+    fontSize: {
+      sm: "0.781rem",
+      base: "0.938rem",
+      xl: "1.125rem",
+      "2xl": "1.35rem",
+      "3xl": "1.87rem",
+      "4xl": "1.944rem",
+      "5xl": "2.333rem",
+    },
+    color: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "hsl(0, 0%, 100%)",
+      darkMagenta: "hsl(300, 43%, 22%)",
+      softPink: "hsl(333, 80%, 67%)",
+      darkGrayishMagenta: "hsl(303, 10%, 53%)",
+      lightGrayishMagenta: "hsl(300, 24%, 96%)",
+    },
     extend: {},
   },
   plugins: [],
