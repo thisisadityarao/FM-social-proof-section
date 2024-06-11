1d6ced1ff484ebf03a6cf7192b8cf855f8cf25ce
Aditya Rao <dev.adityarao@gmail.com>
2024-06-11 12:50:51 +0530

    Add prettierrc

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: ["./index.html"],
   theme: {
     fontFamily: {
       sans: ["League Spartan", "sans-serif"],
-    },
-    fontSize: {
-      sm: "0.781rem",
-      base: "0.938rem",
-      xl: "1.125rem",
-      "2xl": "1.35rem",
-      "3xl": "1.87rem",
-      "4xl": "1.944rem",
-      "5xl": "2.333rem",
     },
     color: {
       transparent: "transparent",
